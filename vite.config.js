07e00542fc2ab9f2cd6323aa88eb564cc2b33cb2
bikash kumar gouda <kumar.bikash.gouda@gmail.com>
2024-09-26 22:12:24 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Dark-Light-Mode/", // Use your repository name here
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // Default output directory for Vite, can change to 'build' if needed
+  },
+});
